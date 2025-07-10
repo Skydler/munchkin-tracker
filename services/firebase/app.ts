251f407fb0bdd2d5ab5c5ba1c25d42340449413d
Leonel Mandarino <leonelmandarino@gmail.com>
2025-07-09 21:24:02 -0300

    Add measurement id and publish script

--- services/firebase/app.ts
+++ services/firebase/app.ts
@@ -2,11 +2,12 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "tabletop-stats-tracker.firebaseapp.com",
-  projectId: "tabletop-stats-tracker",
-  storageBucket: "tabletop-stats-tracker.firebasestorage.app",
-  messagingSenderId: "299521033574",
-  appId: "1:299521033574:web:222c4684b27e9a20e034a7",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
